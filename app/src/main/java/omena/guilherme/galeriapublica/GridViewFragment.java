48869e28dd91aafae2a6c519e8cd7ac16b38ff0e
guihocosta <guiihocosta@gmail.com>
2024-10-15 11:01:53 -0300

    Commit aula 4

--- app/src/main/java/omena/guilherme/galeriapublica/GridViewFragment.java
+++ app/src/main/java/omena/guilherme/galeriapublica/GridViewFragment.java
@@ -2,7 +2,16 @@ package omena.guilherme.galeriapublica;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.paging.PagingData;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +23,9 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class GridViewFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+    private View view;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -38,12 +50,11 @@ public class GridViewFragment extends Fragment {
      */
     // TODO: Rename and change types and number of parameters
     public static GridViewFragment newInstance(String param1, String param2) {
-        GridViewFragment fragment = new GridViewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        return null;
+    }
+
+    public static GridViewFragment newInstance() {
+        return new GridViewFragment();
     }
 
     @Override
@@ -58,7 +69,25 @@ public class GridViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_grid_view, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_grid_view, container, false);
+        return view;
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mViewModel = new ViewModelProvider(getActivity()).get(MainViewModel.class);
+        GridAdapter gridAdapter = new GridAdapter(new ImageDataComparator());
+        LiveData<PagingData<ImageData>> liveData = mViewModel.getPageLv();
+        liveData.observe(getViewLifecycleOwner(), new Observer<PagingData<ImageData>>() {
+            @Override
+            public void onChanged(PagingData<ImageData> imageDataPagingData) {
+                gridAdapter.submitData(getViewLifecycleOwner().getLifecycle(), imageDataPagingData);
+            }
+        });
+
+        RecyclerView rvGallery = (RecyclerView)view.findViewById(R.id.rvGrid);
+        rvGallery.setAdapter(gridAdapter);
+        rvGallery.setLayoutManager(new GridLayoutManager(getContext()));
     }
 }
--- app/src/main/java/omena/guilherme/galeriapublica/ListViewFragment.java
+++ app/src/main/java/omena/guilherme/galeriapublica/ListViewFragment.java
@@ -2,7 +2,15 @@ package omena.guilherme.galeriapublica;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.paging.PagingData;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +22,13 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ListViewFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+    private View view;
+
+    public static ListViewFragment newInstance(){
+        return new ListViewFragment();
+    }
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -58,7 +73,28 @@ public class ListViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_list_view, container, false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_view, container, false);
+        return view;
+}
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mViewModel = new ViewModelProvider(getActivity()).get(MainViewModel.class);
+        ListAdapter listAdapter = new ListAdapter(new ImageDataComparator());
+        LiveData<PagingData<ImageData>> liveData = mViewModel.getPageLv();
+        liveData.observe(getViewLifecycleOwner(), new Observer<PagingData<ImageData>>() {
+            @Override
+            public void onChanged(PagingData<ImageData> imageDataPagingData) {
+                listAdapter.submitData(getViewLifecycleOwner().getLifecycle(), imageDataPagingData);
+            }
+        });
+
+        RecyclerView rvGallery = (RecyclerView)view.findViewById(R.id.rvList);
+        rvGallery.setAdapter(listAdapter);
+        rvGallery.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 }
